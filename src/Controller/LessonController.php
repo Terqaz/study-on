aed90ff33408c4f97aeff0e57d34d56801f4855f
Terqaz <holinvova@gmail.com>
2022-04-17 17:36:32 +0300

    Сделал занятие 2 - Написал фикстуры - Сгенерировал БД - Сгенерировал контроллеры и добавил в них логику - Сделал формы

---
+++ src/Controller/LessonController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Lesson;
+use App\Form\LessonType;
+use App\Repository\LessonRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/lessons")
+ */
+class LessonController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_lesson_index", methods={"GET"})
+     */
+    public function index(LessonRepository $lessonRepository): Response
+    {
+        return $this->render('lesson/index.html.twig', [
+            'lessons' => $lessonRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_lesson_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, LessonRepository $lessonRepository): Response
+    {
+        $lesson = new Lesson();
+        $form = $this->createForm(
+            LessonType::class,
+            $lesson,
+            ['course_id' => (int)$request->query->get('course_id')]
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $lessonRepository->add($lesson);
+
+            return $this->redirectToRoute(
+                'app_course_show',
+                ['id' => $lesson->getCourseId()],
+                Response::HTTP_SEE_OTHER
+            );
+        }
+
+        return $this->renderForm('lesson/new.html.twig', [
+            'lesson' => $lesson,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_lesson_show", methods={"GET"})
+     */
+    public function show(Lesson $lesson): Response
+    {
+        return $this->render('lesson/show.html.twig', [
+            'lesson' => $lesson,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_lesson_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Lesson $lesson, LessonRepository $lessonRepository): Response
+    {
+        $form = $this->createForm(
+            LessonType::class,
+            $lesson,
+            ['course_id' => (int)$lesson->getCourseId()]
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $lessonRepository->add($lesson);
+
+            return $this->redirectToRoute(
+                'app_lesson_show',
+                ['id' => $lesson->getId()],
+                Response::HTTP_SEE_OTHER
+            );
+        }
+
+        return $this->renderForm('lesson/edit.html.twig', [
+            'lesson' => $lesson,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_lesson_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Lesson $lesson, LessonRepository $lessonRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$lesson->getId(), $request->request->get('_token'))) {
+            $lessonRepository->remove($lesson);
+        }
+
+        return $this->redirectToRoute(
+            'app_course_show',
+            ['id' => $lesson->getCourseId()],
+            Response::HTTP_SEE_OTHER
+        );
+    }
+}
